29d4a8eaed0ac0a3fdb6d213caefe7a4642a2482
music <musicsnap@163.com>
2018-05-19 16:21:00 +0800

    first commit

---
+++ src/Application.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Windows
+ * Date: 2018/5/18
+ * Time: 14:09
+ */
+namespace Yume;
+
+class Application{
+
+    /**
+     * Application constructor.
+     */
+    public function __construct()
+    {
+
+    }
+}
